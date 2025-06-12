3decb400567df97eb867c572617372b030cfde14
Kalyani pujari <90515489+Kalyanipujari@users.noreply.github.com>
2025-06-12 08:51:18 +0530

    Changed

--- moviereview/src/main/java/com/example/moviereview/repository/FeedbackRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.moviereview.repository;
-
-import com.example.moviereview.model.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {}
--- moviereview/src/main/java/com/example/moviereview/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.moviereview.repository;
-
-import com.example.moviereview.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-    boolean existsByTitleIgnoreCase(String title);
-}
